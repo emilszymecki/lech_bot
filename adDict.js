978eb1462d80ce2cd3877b92cf6ce8a08f198742
Emil Szymecki <emilszymecki@gmail.com>
2019-10-15 13:37:08 +0200

    start work

---
+++ adDict.js
@@ -0,0 +1,17 @@
+const adDict = {
+    optad: [ 'optad', 'pbjs325474', 'pbjs325474hb', 'optad360' ],
+    yieldBird: [ 'yieldbird' ],
+    ezoic: [ 'ezoic', 'ez_', '_ez', 'ezslot' ],
+    themoneytizer: ["tmzr","themoneytizer"],
+    spolecznosci: [ 'spolecznosci', 'pbsjs' ],
+    yieldlove: [ 'yieldlove', 'pbjsYLHH', 'YLHH' ],
+    adv_media: [ 'advmedia', 'epbjs' ],
+    way2grow: [ 'wtg-ads', 'waytogrow', 'w2g' ],
+    adkontekst: [ 'adkontekst' ],
+    zpr: [ 'pollster', 'idmnet' ],
+    Adform: [ 'adform', '_adform' ],
+    Smart: [ 'smartad', 'sas_' ],
+    Adocean: [ 'adocean', 'aomini' ]
+  };
+
+  module.exports = adDict;
---
+++ index.js
@@ -0,0 +1,90 @@
+const puppeteer = require('puppeteer');
+const prompts = require('prompts');
+const gKeys = require('./gKeysResseler');
+const adDict = require('./adDict');
+
+(async () => {
+	const response = await prompts({
+		type: 'text',
+		name: 'site',
+		message: 'podaj site'
+	});
+
+	const browser = await puppeteer.launch({
+		//slowMo: 500,
+		ignoreHTTPSErrors: true,
+		args: [ '--no-sandbox', '--disable-setuid-sandbox' ]
+	});
+	const page = await browser.newPage();
+
+	await page.setUserAgent(
+		'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.87 Safari/537.36'
+	);
+	await page.setViewport({
+		width: 1680,
+		height: 1050
+	});
+
+	try {
+		await page.goto(`https://${response.site}`, { waitUntil: 'networkidle2' });
+	} catch (error) {
+		await page.goto(`http://${response.site}`, { waitUntil: 'networkidle2' });
+	}
+
+	const concurents = await page.evaluate(
+		({ gKeys, adDict }) => {
+			const searchPartner = (gKeys, adDict) => {
+				var scripts = [ ...document.querySelectorAll('script'), ...document.querySelectorAll('link') ]
+					.map((x) => x.href || x.src)
+					.filter((x) => x)
+					.map((x) => x.toLowerCase());
+				var windowKeys = Object.keys(window).map((x) => x.toLowerCase());
+				var prebidCheck = Object.keys(window)
+					.filter((x) => window[x] instanceof Object)
+					.filter((x) => window[x].hasOwnProperty('requestBids'));
+
+				var partnerChecker = (dict) =>
+					dict.flatMap((x) => [ ...scripts, ...windowKeys ].filter((y) => y.includes(x))).length;
+				return {
+					prebid: {
+						prebidCheck: prebidCheck.length > 0,
+						prebidArr: prebidCheck || [],
+						prebidSSP:
+							prebidCheck.map((x) =>
+								[
+									...new Set(window[x].adUnits.flatMap((x) => x.bids.flatMap((y) => y.bidder)))
+								].toString()
+							) || []
+					},
+					gTag: [ ...new Set(document.body.innerHTML.match(/ca-pub-[0-9]{16}/g)) ].map((x) =>
+						[ x, Object.keys(gKeys).find((key) => gKeys[key].includes(x)) ].toString()
+					),
+					spolecznosci: partnerChecker(adDict.spolecznosci),
+					optad: partnerChecker(adDict.optad),
+					yieldbird: partnerChecker(adDict.yieldBird),
+          ezoic: partnerChecker(adDict.ezoic),
+          themoneytizer: partnerChecker(adDict.themoneytizer),
+					yieldlove: partnerChecker(adDict.yieldlove),
+					adv_media: partnerChecker(adDict.adv_media),
+					way2grow: partnerChecker(adDict.way2grow),
+					adkontekst: partnerChecker(adDict.adkontekst),
+					zpr: partnerChecker(adDict.zpr),
+					SSP: {
+						Adform: partnerChecker(adDict.Adform),
+						Adocean: partnerChecker(adDict.Adocean),
+						Smart: partnerChecker(adDict.Smart)
+					}
+				};
+			};
+
+			return searchPartner(gKeys, adDict);
+		},
+		{ gKeys, adDict }
+	);
+
+	console.log(concurents);
+
+	await page.screenshot({ path: 'buddy-screenshot.png' });
+
+	await browser.close();
+})();
