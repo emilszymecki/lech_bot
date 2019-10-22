8dd9c00bd1df66a6c83aa6614a18c555348f7423
Emil Szymecki <emilszymecki@gmail.com>
2019-10-22 15:07:53 +0200

    first commit

--- adDict.js
+++ adDict.js
@@ -1,19 +1,20 @@
 const adDict = {
-    optad: [ 'optad', 'pbjs325474', 'pbjs325474hb', 'optad360' ],
-    yieldBird: [ 'yieldbird' ],
-    yieldriser: ['yieldriser'],
-    yieldlove: [ 'yieldlove', 'pbjsYLHH', 'YLHH' ],
+    optad: [ 'optad', 'pbjs325474', 'pbjs325474hb', 'optad360', 'OA360' ],
+    yieldbird: [ 'yieldbird', "_YB", "YB_" ],
+    yieldriser: ['yieldriser', "_YR", "YR_","yieldr"],
+    yieldlove: [ 'yieldlove', 'pbjsYLHH', 'YLHH', "_YL", "YL_" ],
     ezoic: [ 'ezoic', 'ez_', '_ez', 'ezslot' ],
     themoneytizer: ["tmzr","themoneytizer"],
-    spolecznosci: [ 'spolecznosci', 'pbsjs' ],
+    spolecznosci: [ 'spolecznosci.net','spolecznosci-','spolecznoscinet', 'pbsjs', '_qasp'],
     adv_media: [ 'advmedia', 'epbjs' ],
     mediafarm : [ 'mediafarm' ],
-    way2grow: [ 'wtg-ads', 'waytogrow', 'w2g', 'wtg' ],
+    way2grow: [ 'wtg-ads', 'waytogrow', 'wtg_adslot' ],
     adkontekst: [ 'adkontekst' ],
-    zpr: [ 'pollster', 'idmnet', 'zpr' ],
+    zpr: [ 'pollster', 'idmnet', 'grupazprmedia','zpr_','zpr-', 'grupazpr'],
     Adform: [ 'adform', '_adform' ],
     Smart: [ 'smartad', 'sas_' ],
-    Adocean: [ 'adocean', 'aomini' ]
+    Adocean: [ 'adocean', 'aomini' ],
+    Criteo: [ 'criteo' ],
   };
 
   module.exports = adDict;
